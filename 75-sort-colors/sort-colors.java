742940f9dd4374d767f9f9064854257d06c6ab40
Ashita <ashitaarora67@gmail.com>
2026-03-30 13:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (71.09%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        //DUTCH NATIONAL FLAG ALGORITHM
+        int low=0,high=nums.length-1; int mid=0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+                
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+        }
+
     }
 }

0505240d5f0cccb2ec770e297e1ad1af52d05b5d
Ashita <ashitaarora67@gmail.com>
2026-07-15 11:41:21 +0530

    Time: 1 ms (83.11%) | Memory: 42.1 MB (96.78%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        //sum of all odd primes n*n
+        //sum of all even primes n(n+1);
+        //gcd = gcd(n*n,n(n+1));
+        // gcd = n(n,n+1);
+        // n and n+1 are co primes so = 1 
+        // n*1 = n 
+        return n;
+    }
+}

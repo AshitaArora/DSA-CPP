3f95c739d33132e41097d5eb9b4b2a34fb4976d1
Ashita <ashitaarora67@gmail.com>
2026-07-20 16:53:47 +0530

    Time: 136 ms (63.42%) | Memory: 56 MB (52.93%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
@@ -1,20 +1,19 @@
 class Solution {
     public List<List<Integer>> findPrimePairs(int n) {
         boolean[] prime = new boolean[n+1];
-        for(int i = 2 ; i < n ; i++ ){
-            prime[i] = true;
+        for(int i=2;i<=n;i++){
+            prime[i]=true;
         }
-        for( int i = 2 ; i*i <= n ; i++){
-            if(prime[i] == true){
-                for( int j = i*i ; j <= n ; j = j+i){
-                    prime[j] = false;
+        for(int i=2;i*i<=n;i++){
+            if(prime[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    prime[j]=false;
                 }
             }
         }
-
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 2; i <= n/2 ; i++){
-            if(prime[i]==true && prime[n-i]==true){
+        for(int i=0;i<=n/2;i++){
+            if(prime[i]==true&&prime[n-i]==true){
                 List<Integer> pair = new ArrayList<>();
                 pair.add(i);
                 pair.add(n-i);

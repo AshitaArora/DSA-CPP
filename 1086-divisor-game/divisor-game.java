8af0b6a1ebfa09a3968b05f2ab52d7f005801574
Ashita <ashitaarora67@gmail.com>
2026-07-20 17:43:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (86.30%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        //basically if alice win then we will return true and alice will only win if the number is even 
+        return n%2==0;
+    }
+}

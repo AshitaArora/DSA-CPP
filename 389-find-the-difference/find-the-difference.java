b202879bf95d071133f2fed80adcff28c79a0864
Ashita <ashitaarora67@gmail.com>
2026-07-22 16:39:34 +0530

    Time: 4 ms (21.06%) | Memory: 43.2 MB (36.15%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,12 +1,26 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char result=0;
+        // char result=0;
+        // for(int i=0;i<s.length();i++){
+        //     result^=s.charAt(i);
+        // }
+        // for(int i=0;i<t.length();i++){
+        //     result^=t.charAt(i);
+        // }
+        // return result;
+
+        //2nd method by frequency array
+        int[] freq = new int[26];
         for(int i=0;i<s.length();i++){
-            result^=s.charAt(i);
+            freq[s.charAt(i)-'a']++;
         }
         for(int i=0;i<t.length();i++){
-            result^=t.charAt(i);
-        }
-        return result;
+            freq[t.charAt(i)-'a']--;
+        
+        if(freq[t.charAt(i)-'a']<0){
+            return t.charAt(i);
+        }}
+        return ' ';
     }
 }
+//used property of xor same characters 0 hogye jo bch gya 1 hua nd return hogya result me

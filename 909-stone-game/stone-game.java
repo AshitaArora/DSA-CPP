fa0bbc4ffdf61dc70952983fb934de5eda45a250
Ashita <ashitaarora67@gmail.com>
2026-08-02 23:02:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (25.83%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
         return true;
+        //this will be true always cuz we have even  number of piles nd odd number of total stones to alice jb b uthaega he will check k odd indexes ka sum jyada aa raha h ya even nd then go accordingly and will win always so true
     }
 }
